53fa3d1e1f013236041f728781b0c5dffb9119f6
uGuinea <guinea.urko@gmail.com>
2015-07-11 23:51:39 +0200

    Primera página y favicon

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+var quizController = require("../controllers/quiz_controller");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Quiz2015' });
 });
+
+router.get('/quizes/question',quizController.question);
+router.get('/quizes/answer',quizController.answer);
+
 
 module.exports = router;
