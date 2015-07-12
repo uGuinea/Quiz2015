ca958221d3a00719274b02437211bbad27ca3526
uGuinea <guinea.urko@gmail.com>
2015-07-12 10:11:33 +0200

    página de créditos

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var quizController = require("../controllers/quiz_controller");
-
+var creditsontroller = require("../controllers/credits_controller");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Quiz2015' });
@@ -10,6 +10,8 @@ router.get('/', function(req, res, next) {
 
 router.get('/quizes/question',quizController.question);
 router.get('/quizes/answer',quizController.answer);
+
+router.get("/author",creditsontroller.author);
 
 
 module.exports = router;
