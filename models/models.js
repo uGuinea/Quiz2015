e0bf3b2c735c6b6089db35178bcbafa2ac766c8a
uGuinea <guinea.urko@gmail.com>
2015-07-16 22:51:11 +0200

    base de datos sqlite

---
+++ models/models.js
@@ -0,0 +1,20 @@
+var path= require("path");
+
+var Sequelize = require('sequelize');
+
+var sequelize = new Sequelize(null,null,null,
+  {dialect:"sqlite",storage:"quiz.sqlite"});
+
+  var Quiz = sequelize["import"](path.join(__dirname,'quiz'));
+
+  exports.Quiz = Quiz;
+
+  sequelize.sync().then(function(){
+    Quiz.count().then(function(count){
+      //if(count===0){
+        Quiz.create({pregunta:'aaaaSCapital de Italiaw',
+        respuesta:"Roma"}
+      ).then(function(){console.log("Base de datos started")});
+      //}
+    });
+  });
